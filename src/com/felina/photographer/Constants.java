6c9ef5e795d410d3ab6be97f134cd1ec88c1c315
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2014-03-20 15:39:11 +0000

    initial commit

---
+++ src/com/felina/photographer/Constants.java
@@ -0,0 +1,11 @@
+package com.felina.photographer;
+
+public final class Constants {
+	public static class Extra {
+		public static String UUID_PREF_FILE = "com.felina.photographer.uuid_pref";
+		public static String UUID_PREF = "uuid_pref";
+		public static String CREDENTIAL_PREF_FILE = "com.felia.photographer.credential_pref";
+		public static String EMAIL_PREF = "email_pref";
+		public static String TOKEN_PREF = "token_pref";
+	}
+}
---
+++ src/com/felina/photographer/UUIDFactory.java
@@ -0,0 +1,16 @@
+package com.felina.photographer;
+
+import java.util.UUID;
+
+import android.content.Context;
+
+public class UUIDFactory {
+	private static UUID uuid;
+	
+	public UUIDFactory(Context context) {
+		
+		if(uuid == null) {
+			
+		}
+	}
+}
